a7a4d62f08bf30a187a3a89d6617a6c595fd8c56
Kevin Isaac Cortez <kenichi003@gmail.com>
2020-07-21 00:15:45 +0800

    changed datasource URL for heroku

--- src/main/java/com/kentech/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/kentech/conferencedemo/config/PersistenceConfiguration.java
@@ -9,14 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PersistenceConfiguration {
 	
-	@Bean
-	public DataSource dataSource() {
-		DataSourceBuilder<?> builder = DataSourceBuilder.create();
-		builder.url("jdbc:mysql://localhost:3306/conference_app?useSSL=false");//this overrides the spring.datasource.url property in the application.properties file
-		builder.username("root");
-		builder.password("root");
-		System.out.println("Persistence configuration flag");
-		return builder.build();
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		DataSourceBuilder<?> builder = DataSourceBuilder.create();
+//		builder.url("jdbc:mysql://localhost:3306/conference_app?useSSL=false");//this overrides the spring.datasource.url property in the application.properties file
+//		builder.username("root");
+//		builder.password("root");
+//		System.out.println("Persistence configuration flag");
+//		return builder.build();
+//	}
 
 }
